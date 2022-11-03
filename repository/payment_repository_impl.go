1f840f9ed53afdc2b069d97a94444e8a78e44782
Naila Febriana Bidrin <nandaherdiansyah11@gmail.com>
2022-11-04 06:38:20 +0700

    routing added

--- repository/payment_repository_impl.go
+++ repository/payment_repository_impl.go
@@ -16,12 +16,12 @@ func NewPaymentRepository() *PaymentRepositoryImpl {
 
 func (repository *PaymentRepositoryImpl) Payment(ctx context.Context, tx *sql.Tx, payment domain.Payment) domain.Payment {
 	SQL := "insert into payments(partner_reference_no, amount, currency) values (?, ?, ?)"
-	result, err := tx.ExecContext(ctx, SQL, payment.PartnerReferenceNo, payment.Amount, payment.Currency)
+	_, err := tx.ExecContext(ctx, SQL, payment.PartnerReferenceNo, payment.Amount, payment.Currency)
 	helper.PanicIfError(err)
 
-	id, err := result.LastInsertId()
+	//id, err := result.LastInsertId()
 	helper.PanicIfError(err)
 
-	payment.PartnerReferenceNo = string(id)
+	//payment.PartnerReferenceNo = string(id)
 	return payment
 }
--- service/payment_service_impl.go
+++ service/payment_service_impl.go
@@ -7,7 +7,11 @@ import (
 	"direct-debit-api/model/domain"
 	"direct-debit-api/model/web"
 	"direct-debit-api/repository"
+	"encoding/json"
+	"fmt"
 	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
 type PaymentServiceImpl struct {
@@ -24,8 +28,8 @@ func NewPaymentService(paymentRepository *repository.PaymentRepositoryImpl, DB *
 	}
 }
 
-func (service *PaymentServiceImpl) Payment(ctx context.Context, request web.PaymentRequest) web.PaymentResponse {
-	err := service.Validate.Struct(request)
+func (service *PaymentServiceImpl) Payment(ctx context.Context, requestClient web.PaymentRequest) web.PaymentResponse {
+	err := service.Validate.Struct(requestClient)
 	helper.PanicIfError(err)
 
 	tx, err := service.DB.Begin()
@@ -33,12 +37,32 @@ func (service *PaymentServiceImpl) Payment(ctx context.Context, request web.Paym
 	defer helper.CommitOrRollback(tx)
 
 	payment := domain.Payment{
-		PartnerReferenceNo: request.PartnerReferenceNo,
-		Amount:             request.Amount,
-		Currency:           request.Currency,
+		PartnerReferenceNo: requestClient.PartnerReferenceNo,
+		Amount:             requestClient.Amount,
+		Currency:           requestClient.Currency,
 	}
 
 	payment = service.PaymentRepository.Payment(ctx, tx, payment)
+
+	router := httprouter.New()
+	router.GET("/images/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		//image := params.ByName("image")
+		//text := "Image : " + image
+
+		//fmt.Fprintln(writer, requestClient.PartnerReferenceNo)
+		//fmt.Fprintln(writer, requestClient.Amount)
+		//fmt.Fprintln(writer, requestClient.Currency)
+		bytes, _ := json.Marshal(payment)
+		fmt.Fprint(writer, string(bytes))
+
+	})
+	//fmt.Println(payment)
+	server := http.Server{
+		Handler: router,
+		Addr:    "localhost:4000",
+	}
+
+	server.ListenAndServe()
 
 	return helper.ToPaymentResponse(payment)
 }
